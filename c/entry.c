8d7d4efdd0515b31ad357554e1d0632e82e98afb
Xuejie Xiao <xxuejie@gmail.com>
2019-06-27 09:06:44 +0000

    chore: clang-format

--- c/entry.c
+++ c/entry.c
@@ -1,38 +1,38 @@
-#include "duktape.h"
 #include "ckb_syscalls.h"
+#include "duktape.h"
 
 duk_double_t dummy_get_now(void) {
-	/*
+  /*
    * Return a fixed time here as a dummy value since CKB does not support
    * fetching current timestamp
    */
-	return -11504520000.0;
+  return -11504520000.0;
 }
 
 static duk_ret_t duk_ckb_debug(duk_context *ctx) {
-	duk_push_string(ctx, " ");
-	duk_insert(ctx, 0);
-	duk_join(ctx, duk_get_top(ctx) - 1);
+  duk_push_string(ctx, " ");
+  duk_insert(ctx, 0);
+  duk_join(ctx, duk_get_top(ctx) - 1);
   ckb_debug(duk_safe_to_string(ctx, -1));
-	return 0;
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-	duk_context *ctx = duk_create_heap_default();
+  duk_context *ctx = duk_create_heap_default();
 
-	duk_push_c_function(ctx, duk_ckb_debug, DUK_VARARGS);
-	duk_put_global_string(ctx, "ckb_debug");
+  duk_push_c_function(ctx, duk_ckb_debug, DUK_VARARGS);
+  duk_put_global_string(ctx, "ckb_debug");
 
   if (argc == 2) {
     duk_eval_string(ctx, argv[1]);
-    duk_pop(ctx);  /* pop eval result */
+    duk_pop(ctx); /* pop eval result */
   } else if (argc == 3) {
     /* TODO: load source from one of the dep */
   } else {
     return -1;
   }
 
-	duk_destroy_heap(ctx);
+  duk_destroy_heap(ctx);
 
-	return 0;
+  return 0;
 }
